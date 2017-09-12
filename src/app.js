772da2b324de25697011e3c53a4b161167d58018
u4bi <u4bi@u4bi.com>
2017-09-12 11:33:12 +0900

    conversion list logic render

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 onload = () => {
 
-    const [ send, output, image ] = document.querySelectorAll('.box');
+    const [ send, output, image, allRemove, todos ] = document.querySelectorAll('.box');
     
     send.focus();
     
@@ -8,14 +8,28 @@ onload = () => {
 
         if('https://drive.google.com/file/d/' !== send.value.substring(0,32)) return;
         
-        output.value = `https://drive.google.com/uc?export=view&id=${ send.value.substring(32, 60) }`; 
-        output.focus();
-        output.setSelectionRange(0 , output.value.length);
-        
-        send.value = '';
+        output.value = `https://drive.google.com/uc?export=view&id=${ send.value.substring(32, 60) }`;
+        view(output);
 
-        image.style.backgroundImage = `url('${ output.value }`;
+        send.value = '';
+        
+        let entry = document.createElement('p');        
+        
+        entry.innerHTML = `<input readOnly size="18" value="${ output.value }"><span> X </span>`;
+        todos.appendChild(entry);
+
+        entry.firstChild.addEventListener('click', (e) => view(entry.firstChild) );
+        entry.lastChild.addEventListener('click', () => entry.lastChild.parentElement.parentElement.removeChild(entry.lastChild.parentElement));
 
     });
-    
+
+    allRemove.addEventListener('click', () => Object.keys(todos.children).map( (entry) => todos.removeChild(todos.children[0]) ) );
+
+    const view = (element) => {
+        element.focus();
+        
+        element.setSelectionRange(0 , element.value.length);
+        image.style.backgroundImage = `url('${ element.value }`;        
+    };
+
 };
